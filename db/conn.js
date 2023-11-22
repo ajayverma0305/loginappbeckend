c068980b059db0ddb50815cb45abbb5683d06ef4
ajayverma05 <96461634+ajayverma05@users.noreply.github.com>
2023-11-22 17:23:29 +0530

    first commit

---
+++ db/conn.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+
+const DB = process.env.DATABASE
+
+mongoose.connect(DB,{
+}).then(()=> console.log("DataBase Connected")).catch((errr)=>{
+    console.log(errr);
+})
